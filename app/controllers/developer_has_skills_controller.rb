930ef657f1347ba73deea709add670fd4784a46f
Carlos Olivares <carlos.olivares@admios-sa.com>
2015-03-04 18:13:26 -0500

    Finished the relation between Developer and Skills

--- app/controllers/developer_has_skills_controller.rb
+++ app/controllers/developer_has_skills_controller.rb
@@ -4,17 +4,61 @@ class DeveloperHasSkillsController < ApplicationController
 
   def index
   	 @developer = Person.find(params[:developer_id])
-     @developer_skills = PersonHasSkill.where("person_id = ?" , params[:developer_id])
+     @developer_skills = PersonHasSkill.list_developer_skills(params[:developer_id])
+     @developer_assigned_skills = PersonHasSkill.list_of_valid_skills(@developer_skills)
+     
+
   end
 
   def edit
+     @developer = Person.find(params[:developer_id])
+     @developer_has_skills = PersonHasSkill.find(params[:id])
+
   end
 
   def new
      @developer = Person.find(params[:developer_id])
-     @skills = Skill.all
+     @developer_skills = PersonHasSkill.list_developer_skills(params[:developer_id])
+     @developer_assigned_skills = PersonHasSkill.list_of_valid_skills(@developer_skills)
+
+
   end
 
   def show
   end
+
+  def create
+    @developer_skill = PersonHasSkill.new(secure_params)
+    @developer_skill.person_id = params[:developer_id]
+    if @developer_skill.save
+      redirect_to developer_developer_has_skills_path,   :notice => "Skill Association Created!"
+    else
+      redirect_to new_developer_developer_has_skill_path, :alert => "Unable to Associate Skill."
+    end
+  end
+
+
+  def update
+     @developer_skill = PersonHasSkill.find(params[:id])
+    if @developer_skill.update_attributes(secure_params)
+      redirect_to developer_developer_has_skills_path, :notice => "Skill Association updated."
+    else
+      redirect_to new_developer_developer_has_skill_path, :alert => "Unable to update Association."
+    end
+  end
+
+
+  def destroy
+    developer_skill = PersonHasSkill.find(params[:id])
+    developer_skill.destroy
+    redirect_to developer_developer_has_skills_path, :notice => "Association deleted."
+  end
+
+
+
+  def secure_params
+    params.require(:developer_has_skills).permit(:person_id, :skill_id, :rating, :projects, :note)
+  end
+
+
 end
--- app/models/person_has_skill.rb
+++ app/models/person_has_skill.rb
@@ -2,11 +2,32 @@ class PersonHasSkill < ActiveRecord::Base
   belongs_to :person
   belongs_to :skill
 
+  enum skill_rating: ['never touched it', 'read about it and used it privately', 'learning, using it in a current project', 'completed 1 project with it', 'completed 2-3 projects with it', 'completed more than 3 projects']
 
 
-# def list_developer_skills(dev)
-# 	PersonHasSkill.where()
-# end
 
+
+ #validates_presence_of :name
+
+	def self.list_developer_skills(dev)
+		PersonHasSkill.where("person_id = ?" , dev)
+	end
+
+
+
+	def self.list_of_valid_skills(devs_object)
+
+		not_in = []
+		devs_object.each do |dev_skill|
+			not_in.push(dev_skill.skill_id)
+		end
+		
+		unless not_in.first.blank?
+		     Skill.where.not('id in (?)', not_in )
+		else
+			 Skill.all
+		end
+
+	end
 
 end
