fd2fae6d72d2bf88aac02e72812f194e59f7d5ab
Rafael Williams <rafael.williams@gmail.com>
2015-05-07 06:27:53 -0500

    add rule to current project add tooltip to developer project view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
   	# devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :last_name, :address, :phone,:cell_phone, :role, :image) }
  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password,:name, :last_name, :address, :phone,:cell_phone, :role, :image) }
   end
+
+  private
+  def set_current_project
+    params[:person_has_project][:current_project] = true if params[:person_has_project][:finish_date].to_date > Date.today
+    params[:person_has_project][:current_project] = false unless params[:person_has_project][:finish_date].to_date > Date.today
+  end
 end
